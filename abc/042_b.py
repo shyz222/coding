3f03547c8c2995067e4c36b7093c14f3530a032e
shyz222 <shuhei0107ss@gmail.com>
2022-03-27 19:27:56 +0900

    update

---
+++ abc/042_b.py
@@ -0,0 +1,8 @@
+n,l=map(int,input().split())
+s = sorted([input() for _ in range(n)])
+print(''.join(s))
+
+# n,l=map(int,input().split())
+# s = [input() for _ in range(n)]
+# s.sort
+# print(''.join(s))
